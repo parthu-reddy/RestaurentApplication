f1b4994cc19a72d533a7fb18feaefdf1c44e3316
Parthu <parthudhanireddy@gmail.com>
2026-08-26 22:12:44 +0530

    Fix OpenAPI generation test and contract test stability

--- src/test/java/com/fooddelivery/OpenApiGenerationTest.java
+++ src/test/java/com/fooddelivery/OpenApiGenerationTest.java
@@ -135,7 +135,9 @@ public class OpenApiGenerationTest {
     @org.springframework.boot.test.mock.mockito.MockBean
     private com.fooddelivery.restaurant.client.AdvertisementClient advertisementClient;
 
-    @org.springframework.boot.autoconfigure.SpringBootApplication(scanBasePackages = {"com.fooddelivery.restaurant.controller"}, excludeName = {"org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration", "org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration", "org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration", "org.springframework.boot.actuate.autoconfigure.security.reactive.ManagementReactiveSecurityAutoConfiguration", "org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration"})
+    @org.springframework.context.annotation.Configuration
+    @org.springframework.context.annotation.ComponentScan(basePackages = {"com.fooddelivery.restaurant.controller"})
+    @org.springframework.boot.autoconfigure.EnableAutoConfiguration(excludeName = {"org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration", "org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration", "org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration", "org.springframework.boot.actuate.autoconfigure.security.reactive.ManagementReactiveSecurityAutoConfiguration", "org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration"})
     static class TestApp {
     }
 
