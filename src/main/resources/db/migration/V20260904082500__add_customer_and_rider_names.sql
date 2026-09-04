cd7e644d6dcf2a832213b6564ddc62c7d23df28b
Parthu <parthudhanireddy@gmail.com>
2026-09-04 08:36:13 +0530

    Fix migration script to only add customer_id

--- src/main/resources/db/migration/V20260904082500__add_customer_and_rider_names.sql
+++ src/main/resources/db/migration/V20260904082500__add_customer_and_rider_names.sql
@@ -1,3 +1,1 @@
 ALTER TABLE restaurant_orders ADD COLUMN customer_id UUID;
-ALTER TABLE restaurant_orders ADD COLUMN customer_name VARCHAR(255);
-ALTER TABLE restaurant_orders ADD COLUMN rider_name VARCHAR(255);
